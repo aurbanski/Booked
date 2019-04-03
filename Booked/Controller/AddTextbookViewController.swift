ea186a6322fd9425d6fcbe0d9e562cfda5dcb05d
Alex Urbanski <alexurbanski@Alexs-iMac.local>
2019-04-02 21:31:37 -0600

    Got the images to upload to storage/basic storage for postings

---
+++ Booked/Controller/AddTextbookViewController.swift
@@ -0,0 +1,85 @@
+//
+//  AddTextbookController.swift
+//  Booked
+//
+//  Created by Alex Urbanski on 4/2/19.
+//  Copyright © 2019 Alex Urbanski. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AddTextbookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var ISBNTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var conditionTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    var uploadImage: Data?
+    
+    func toggleEnabledViews(_ option: Bool) {
+        titleTextField.isEnabled = option
+        authorTextField.isEnabled = option
+        ISBNTextField.isEnabled = option
+        priceTextField.isEnabled = option
+        conditionTextField.isEnabled = option
+        submitButton.isEnabled = option
+    }
+    
+    @IBAction func imageButtonPressed(_ sender: Any) {
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            uploadImage = image.pngData()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        toggleEnabledViews(false)
+        let uuid = NSUUID().uuidString + ".png"
+        let storage = Storage.storage().reference().child(uuid)
+        var downloadURL: String?
+        
+        if uploadImage != nil {
+            storage.putData(uploadImage!, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error!)
+                } else {
+                    Storage.storage().reference().child(uuid).downloadURL { (url, error) in
+                        if error != nil {
+                            print(error!)
+                        } else {
+                            downloadURL = url?.absoluteString
+                            let listingsDB = Database.database().reference().child("Postings")
+                            let posting = ["Title": self.titleTextField.text!, "Author": self.authorTextField.text!, "ISBN": self.ISBNTextField.text!, "Price": self.priceTextField.text!, "Condition": self.conditionTextField.text!, "Seller": Auth.auth().currentUser?.email!, "ImageURL": downloadURL!]
+                            
+                            listingsDB.childByAutoId().setValue(posting) {
+                                (error, reference) in
+                                if error != nil {
+                                    print(error!)
+                                } else {
+                                    print("Posting inserted in database")
+                                    self.toggleEnabledViews(true)
+                                    self.titleTextField.text = ""
+                                    self.authorTextField.text = ""
+                                    self.ISBNTextField.text = ""
+                                    self.priceTextField.text = ""
+                                    self.conditionTextField.text = ""
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Booked/View/Custom Views/TextbookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextbookTableViewCell.swift
+//  Booked
+//
+//  Created by Alex Urbanski on 4/2/19.
+//  Copyright © 2019 Alex Urbanski. All rights reserved.
+//
+
+import UIKit
+
+class TextbookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
