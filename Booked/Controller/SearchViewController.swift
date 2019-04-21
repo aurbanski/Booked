9d0c7706373f5789c63d93267370b47c160dc3f9
Alex Urbanski <alexurbanski@Alexs-iMac.local>
2019-04-21 17:30:44 -0600

    Pushing lots of code

---
+++ Booked/Controller/SearchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SearchViewController.swift
+//  Booked
+//
+//  Created by Alex Urbanski on 4/21/19.
+//  Copyright © 2019 Alex Urbanski. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
